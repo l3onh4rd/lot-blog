c4653179e57fe313751e3ff71064a0d01a9c29a3
l3onh4rd <46918465+l3onh4rd@users.noreply.github.com>
2022-08-05 22:30:34 +0200

    added google analytics code

--- main.ts
+++ main.ts
@@ -10,25 +10,16 @@ blog({
     { title: "Email", url: "mailto:leoofficetutorials@gmail.com" },
     { title: "YouTube", url: "https://youtube.com/Leoofficetutorials" },
     { title: "LinkedIn", url: "https://www.linkedin.com/in/leo-office-tutorials-937104217"},
-    {title: "Twitter", url: "https://twitter.com/LeoTutorials"},
-    {title: "Facebook", url: "https://business.facebook.com/Leo-Office-Tutorials-228106565837208/"},
-    {title: "TikTok", url: "https://www.tiktok.com/@leoofficetutorials"},
-    {title: "Instagram", url: "https://www.instagram.com/leo_officetutorials/"}
+    { title: "Twitter", url: "https://twitter.com/LeoTutorials"},
+    { title: "Facebook", url: "https://business.facebook.com/Leo-Office-Tutorials-228106565837208/"},
+    { title: "TikTok", url: "https://www.tiktok.com/@leoofficetutorials"},
+    { title: "Instagram", url: "https://www.instagram.com/leo_officetutorials/"}
   ],
   lang: "de",
   dateStyle: "long",
   favicon: "favicon.ico",
-
-  // middlewares: [
-
+  middlewares: [
     // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
-
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
-
-  // ]
+    ga(String(Deno.env.get("GA_CODE"))),
+  ]
 });
