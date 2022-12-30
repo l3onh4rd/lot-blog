d3e5eaab5029206aa4eed83f3b81aab36be840e8
l3onh4rd <leonhardkrause1999@gmail.com>
2022-12-30 15:33:25 +0100

    updated blog lib

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import blog, { ga, redirects } from "https://deno.land/x/blog@0.4.2/blog.tsx";
+import blog, { ga, redirects } from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
   title: "Leo Office Tutorials",
